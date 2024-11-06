99b150bc7bc869b84151550cd341f695aaab2f8a
HERNANDIITO <pablohhgg@gmail.com>
2024-11-06 08:54:13 +0100

    FUERA LA BASURA

--- Workspace/GameAffinityGen/GameAffinityGen.Infraestructure/obj/Debug/net8.0/GameAffinityGen.Infraestructure.AssemblyInfo.cs
+++ Workspace/GameAffinityGen/GameAffinityGen.Infraestructure/obj/Debug/net8.0/GameAffinityGen.Infraestructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameAffinityGen.Infraestructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d3d3149f7ed3207c92ad8bff2edada3fbe09658")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a9ff18699cc2f724943ef1a3974c2ece0c17160")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameAffinityGen.Infraestructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameAffinityGen.Infraestructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
