c9b55fc942bd485352a707a5cb24a09a71f9689f
davidxx04 <152333556+davidxx04@users.noreply.github.com>
2024-11-07 13:03:52 +0100

    banear usuario que no se puede hacer y hay que replantearlo o dejarlo

--- Workspace/GameAffinityGen/GameAffinityGen.ApplicationCore/CEN/manual/ModeradorCEN_banear_usuarios.cs
+++ Workspace/GameAffinityGen/GameAffinityGen.ApplicationCore/CEN/manual/ModeradorCEN_banear_usuarios.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using GameAffinityGen.ApplicationCore.Exceptions;
 using GameAffinityGen.ApplicationCore.EN.GameAffinity;
 using GameAffinityGen.ApplicationCore.IRepository.GameAffinity;
+using GameAffinityGen.ApplicationCore.CP.GameAffinity;
 
 
 /*PROTECTED REGION ID(usingGameAffinityGen.ApplicationCore.CEN.GameAffinity_Moderador_banear_usuarios) ENABLED START*/
@@ -20,6 +21,7 @@ namespace GameAffinityGen.ApplicationCore.CEN.GameAffinity
             /*PROTECTED REGION ID(GameAffinityGen.ApplicationCore.CEN.GameAffinity_Moderador_banear_usuarios) ENABLED START*/
 
             // Write here your custom code...
+            this
 
             RegistradoEN listillo = this.GetByOID(user_ID);
 
@@ -28,6 +30,8 @@ namespace GameAffinityGen.ApplicationCore.CEN.GameAffinity
                 throw new Exception("ID del usuario no existente");
             }
 
+            //ahora habria que dar de baja pero el programa no hace caso asi que o se hace otra cosa o yoquese
+            
             
 
             /*PROTECTED REGION END*/
