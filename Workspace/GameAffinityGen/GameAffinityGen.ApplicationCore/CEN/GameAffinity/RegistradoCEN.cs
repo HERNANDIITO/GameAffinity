88a983acd701ce1f3e95f5cc801412c0568b4c6a
HERNANDIITO <pablohhgg@gmail.com>
2024-12-12 13:46:14 +0100

    correcciones del registrado cen

--- Workspace/GameAffinityGen/GameAffinityGen.ApplicationCore/CEN/GameAffinity/RegistradoCEN.cs
+++ Workspace/GameAffinityGen/GameAffinityGen.ApplicationCore/CEN/GameAffinity/RegistradoCEN.cs
@@ -45,15 +45,12 @@ public void Dejar_de_seguir_perfiles (int p_Registrado_OID, System.Collections.G
 }
 public void Aceptar_mentoria (int registrado_oid)
 {
-        RegistradoEN registradoEN = null;
-
-        //Initialized RegistradoEN
-        registradoEN = new RegistradoEN ();
-        registradoEN.Id = registrado_oid;
-        //Call to RegistradoRepository
-
-        _IRegistradoRepository.Aceptar_mentoria (registradoEN);
-}
+            /*PROTECTED REGION ID(GameAffinityGen.ApplicationCore.CEN.GameAffinity_Registrado_Aceptar_mentoria) ENABLED START*/
+            RegistradoEN registradoEN = this.GetByOID(registrado_oid);
+            registradoEN.Es_mentor = true;
+            _IRegistradoRepository.ModifyDefault(registradoEN);
+            /*PROTECTED REGION END*/
+        }
 
 public int New_ (string p_nombre, string p_email, string p_nick, bool p_es_mentor, bool p_notificaciones, String p_contrasenya, string p_img)
 {
