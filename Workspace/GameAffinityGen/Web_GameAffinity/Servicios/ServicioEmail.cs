054046de3730237d1561c01fb60d9c1e76adf0fa
Pelee4 <148076585+Pelee4@users.noreply.github.com>
2024-12-18 14:40:05 +0100

    corregido email

--- Workspace/GameAffinityGen/Web_GameAffinity/Servicios/ServicioEmail.cs
+++ Workspace/GameAffinityGen/Web_GameAffinity/Servicios/ServicioEmail.cs
@@ -28,7 +28,7 @@ namespace Web_GameAffinity.Servicios
 
             var smtpClient = new SmtpClient(host, puerto);
             smtpClient.EnableSsl = true;
-            smtpClient.UseDefaultCredentials = true;
+            smtpClient.UseDefaultCredentials = false;
 
             smtpClient.Credentials = new NetworkCredential(emailEmisor, password);
             var mensaje = new MailMessage(emailEmisor!, emailReceptor, tema, cuerpo);
